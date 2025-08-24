5b67731ecfce1fc741f038e1585b6cf6f01e8a47
aleexst99 <alexsateru@gmail.com>
2025-08-24 16:59:36 +0200

    Feature.Landing page acabada + primera integracion leaflet.

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DescriptionComponent } from './description/description.component';
 import { UsersComponent } from './users/users.component';
+import {LeafletMapComponent } from '../../leaflet-map/leaflet-map.component'
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [NavbarComponent,DescriptionComponent,UsersComponent],
+  imports: [NavbarComponent, DescriptionComponent, UsersComponent, LeafletMapComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
