18921347dda0cf42554350a059bf271c48b083b3
aleexst99 <alexsateru@gmail.com>
2025-10-26 12:29:56 +0100

    Fix.Reorganización carpetas, nueva ruta users id añadida(con lazy loading) + limpieza de (componentes y ficheros de configuracion).

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,13 +1,12 @@
+import { UsuariosComponent } from './usuarios/usuarios.component';
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DescriptionComponent } from './description/description.component';
-import { UsersComponent } from './users/users.component';
-import {LeafletMapComponent } from '../../leaflet-map/leaflet-map.component'
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [NavbarComponent, DescriptionComponent, UsersComponent, LeafletMapComponent],
+  imports: [NavbarComponent, DescriptionComponent, UsuariosComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
