cdd2c260f6d39a7f86565b0b3f2099315602973e
aleexst99 <alexsateru@gmail.com>
2025-10-31 19:33:16 +0100

    Feature.Users-profile v1.

--- src/app/features/usuario-perfil/models/user.model.ts
+++ src/app/features/usuario-perfil/models/user.model.ts
@@ -1,6 +1,16 @@
+import { Viaje } from '../viajes/models/viajes.model';
+
+export interface SocialLinks {
+  github: string;
+  linkedin: string;
+}
+
 export interface User {
   id: number;
   name: string;
-  avatar_url: string;
+  photo: string;
   bio: string;
+  social: SocialLinks;
+  trips: Viaje[];
+  wishlist: Viaje[];
 }
--- src/app/features/usuario-perfil/usuario-perfil.component.ts
+++ src/app/features/usuario-perfil/usuario-perfil.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-usuario-perfil',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './usuario-perfil.component.html',
-  styleUrl: './usuario-perfil.component.scss'
+  styleUrls: ['./usuario-perfil.component.scss']
 })
 export class UsuarioPerfilComponent {
+  private route = inject(ActivatedRoute);
+  private http = inject(HttpClient);
 
+  user: User | null = null;
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.http.get<User[]>('../../../assets/data/users.json').subscribe(users => {
+      this.user = users.find(u => u.id === id) ?? null;
+    });
+  }
 }
