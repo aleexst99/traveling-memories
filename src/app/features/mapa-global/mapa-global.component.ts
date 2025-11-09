fe1568e57a9ad691625e6b594155df7d5db338b0
aleexst99 <alexsateru@gmail.com>
2025-11-09 12:28:42 +0100

    Fix.Limpieza código + lazy loading excepto landing page(necesario que no lo sea) + reestructuracion navbar + componente mapa-global añadido.

---
+++ src/app/features/mapa-global/mapa-global.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-global',
+  imports: [],
+  templateUrl: './mapa-global.component.html',
+  styleUrl: './mapa-global.component.scss'
+})
+export class MapaGlobalComponent {
+
+}
