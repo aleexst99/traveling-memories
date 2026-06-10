f7d9fed6f45d122a6f213cc10a4eb46881464b1e
aleexst99 <alexsateru@gmail.com>
2026-06-10 09:23:50 +0200

    ci: add karma.conf.js with ChromeHeadlessCI custom launcher

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    jasmineHtmlReporter: { suppressAll: true },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/travel-blog'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    // Launcher para entornos CI (sin sandbox)
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+      },
+    },
+    restartOnFileChange: true,
+  });
+};
